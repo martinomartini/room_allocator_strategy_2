4ec9f027b7e867317f729ca7ccfa15000f97d9ce
Martini <Martini.Martino@kpmg.nl>
2025-05-27 16:17:17 +0200

    Describe what you changed here

--- app.py
+++ app.py
@@ -449,7 +449,7 @@ with st.expander("🔐 Admin Controls"):
         st.error("❌ Incorrect password.")
 
 # --- Team Form ---
-st.header("Request project room for teams of 3 or more for the week of 26 May - to be filled in between Wednesday 21 May 09:00 until Thursday 22 May 16:00 (Engagement managers only)")
+st.header("Request project room for teams of 3 or more for the week of 2 June - to be filled in between Wednesday 28 May 09:00 until Thursday 29 May 16:00 (Engagement managers only)")
 with st.form("team_form"):
     name = st.text_input("Team Name")
     contact = st.text_input("Contact Person")
@@ -465,7 +465,7 @@ with st.form("team_form"):
             st.success("✅ Submitted!")
 
 # --- Oasis Form ---
-st.header("Reserve Oasis Seat for the week of 26 May - Sumbit preference between Wednesday 21 May 09:00 until Friday 23 May 16:00 - only if you are not in the project of 3 or more")
+st.header("Reserve Oasis Seat for the week of 2 June - Sumbit preference between Wednesday 28 May 09:00 until Friday 30 May 16:00 - only if you are not in the project of 3 or more")
 with st.form("oasis_form"):
     person = st.text_input("Your Name")
     selected_days = st.multiselect(
